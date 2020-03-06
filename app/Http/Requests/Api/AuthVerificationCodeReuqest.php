e3b1158b3aed355a69b510b0efc23c5aca16b5ab
vagrant <vagrant@homestead>
2020-03-06 07:42:34 +0000

    password confirm

--- app/Http/Requests/Api/AuthVerificationCodeReuqest.php
+++ app/Http/Requests/Api/AuthVerificationCodeReuqest.php
@@ -17,7 +17,6 @@ class AuthVerificationCodeReuqest extends FormRequest
                 'required',
                 'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
             ],
-            'password' => 'required|alpha_dash|min:6',
             'verification_key' => 'required|string',
             'verification_code' => 'required|string',
         ];
