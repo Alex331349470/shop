169249c0118b8628f45116a78da07ebaff0a33ce
vagrant <vagrant@homestead>
2020-03-06 06:30:24 +0000

    user login

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\Api\AuthorizationRequest;
+use App\Http\Requests\Api\AuthCaptchaReuqest;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 
 class AuthorizationsController extends Controller
 {
-    public function store(AuthorizationRequest $request)
+    public function store(AuthCaptchaReuqest $request)
     {
         $captchaData = \Cache::get($request->captcha_key);
 
@@ -22,7 +22,14 @@ class AuthorizationsController extends Controller
             throw new AuthenticationException('验证码错误');
         }
 
-        $credentials['phone'] = $request->phone;
+        $username = $request->username;
+
+        if (is_numeric($username)){
+            $credentials['phone'] = $username;
+        } else {
+            $credentials['name'] = $username;
+        }
+
         $credentials['password'] = $request->password;
 
         if (!$token = \Auth::guard('api')->attempt($credentials)) {
@@ -32,9 +39,29 @@ class AuthorizationsController extends Controller
         return $this->respondWithToken($token)->setStatusCode(201);
     }
 
-    public function smsStore()
+    public function smsStore(AuthorizationRequest $request)
     {
-        
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!$verifyData){
+            abort(403,'验证码失效');
+        }
+
+        if (!hash_equals($verifyData['code'],$request->verification_code)){
+            throw new AuthenticationException('验证码不正确');
+        }
+
+        if (!hash_equals($verifyData['phone'],$request->phone)){
+            throw new AuthenticationException('手机号错误');
+        }
+        $credentials['phone'] = $request->phone;
+        $credentials['password'] = $request->password;
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            throw new AuthenticationException('用户名或密码错误');
+        }
+
+        return $this->respondWithToken($token)->setStatusCode(201);
     }
 
     public function update()
