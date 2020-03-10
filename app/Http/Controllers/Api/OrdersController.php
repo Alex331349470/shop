50c9ec763f3c88a52d4c9120c63ff0a33d27bd38
vagrant <vagrant@homestead>
2020-03-10 02:39:22 +0000

    order commit

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\OrderRequest;
+use App\Http\Resources\OrderResource;
 use App\Models\Good;
 use App\Models\UserAddress;
 use App\Models\Order;
@@ -15,7 +16,8 @@ class OrdersController extends Controller
     public function store(OrderRequest $request)
     {
         $user = $request->user();
-        $order = DB::transaction(function () use ($user, $request) {
+        $good_ids = explode(',',$request->good_ids);
+        $order = DB::transaction(function () use ($user, $request, $good_ids) {
             $address = UserAddress::find($request->input('address_id'));
             // 更新此地址的最后使用时间
             $address->update(['last_used_at' => Carbon::now()]);
@@ -38,26 +40,37 @@ class OrdersController extends Controller
             $totalAmount = 0;
             $items = $request->input('items');
             // 遍历用户提交的 good
-            foreach ($items as $data) {
-                $good = Good::find($data['good_id']);
+            foreach ($good_ids as $good_id) {
+                if (!$good = Good::find($good_id)) {
+                    abort(403,$good->title.'不存在该商品');
+                }
+
+                if (!$good->on_sale) {
+                    abort(403,$good->title.'未上架');
+                }
+
+                if ($good->stock === 0) {
+                    abort(403,$good->tile.'库存为零');
+                }
                 // 创建一个 OrderItem 并直接与当前订单关联
                 $item = $order->items()->make([
-                    'amount' => $data['amount'],
-                    'price' => $good->price,
+                    'amount' => $amount = $user->cartItems()->where('good_id',$good_id)->first()->amount,
+                    'price' => $$good->price,
                 ]);
                 $item->good()->associate($good);
                 $item->save();
-                $totalAmount += $good->price * $data['amount'];
+                $totalAmount += $good->price * $amount;
             }
 
             // 更新订单总金额
             $order->update(['total_amount' => $totalAmount]);
 
             // 将下单的商品从购物车中移除
-            $good_ids = collect($items)->pluck('good_id');
             $user->cartItems()->whereIn('good_id', $good_ids)->delete();
 
             return $order;
         });
+
+        return new OrderResource($order);
     }
 }
