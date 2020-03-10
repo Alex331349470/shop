f8ab2e32bf47aecc8bd22a087d527f5650c12afc
vagrant <vagrant@homestead>
2020-03-10 08:40:10 +0000

    qrcode wechat

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -4,30 +4,35 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use Endroid\QrCode\QrCode;
 use Illuminate\Http\Request;
 
 class PaymentsController extends Controller
 {
     public function payByWechat(Order $order, Request $request)
     {
-        if (!($request->type == 'wechat')){
-            abort(403,'支付类型错误');
+        if (!($request->type == 'wechat')) {
+            abort(403, '支付类型错误');
         }
         if ($order->paid_at || $order->closed) {
             abort(403, '订单状态不正确');
         }
 
-        return app('wechat_pay')->scan([
+        $wechatOrder = app('wechat_pay')->scan([
             'out_trade_no' => $order->no,
             'total_fee' => $order->total_amount * 100,
-            'body' => '支付订单:'.$order->no,
+            'body' => '支付订单:' . $order->no,
         ]);
+
+        $qrcode = new QrCode($wechatOrder->code_url);
+
+        return response($qrcode->writeString(), 200, ['Content-Type' => $qrCode->getContentType()]);
     }
 
     public function wechatNotify()
     {
         // 校验回调参数是否正确
-        $data  = app('wechat_pay')->verify();
+        $data = app('wechat_pay')->verify();
         // 找到对应的订单
         $order = Order::where('no', $data->out_trade_no)->first();
         // 订单不存在则告知微信支付
@@ -42,9 +47,9 @@ class PaymentsController extends Controller
 
         // 将订单标记为已支付
         $order->update([
-            'paid_at'        => Carbon::now(),
+            'paid_at' => Carbon::now(),
             'payment_method' => 'wechat',
-            'payment_no'     => $data->transaction_id,
+            'payment_no' => $data->transaction_id,
         ]);
 
         return app('wechat_pay')->success();
