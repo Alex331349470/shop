a8be9901ab2fda0b1432e28591d899d739677e72
vagrant <vagrant@homestead>
2020-03-10 08:21:32 +0000

    wechat pay

--- routes/api.php
+++ routes/api.php
@@ -123,6 +123,7 @@ Route::prefix('v2.0.0')->namespace('Api')->name('api.v2.0.0')->group(function ()
 
                 Route::get('orders/{order}','OrdersController@show')
                     ->name('orders.show');
+
                 Route::post('payment/{order}/wechat','PaymentsController@payByWechat')
                     ->name('payment.wechat');
             });
