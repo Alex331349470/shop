801456dedc6860d4b2f0446f83ee3f7365ca62ec
vagrant <vagrant@homestead>
2020-03-19 07:14:07 +0000

    add category item to good

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -16,7 +16,7 @@ class OrdersController extends Controller
 {
     public function index(Request $request)
     {
-        $orders = Order::where('user_id', $request->user()->id)->with('user', 'items.good.images')->orderBy('created_at', 'desc')->get();
+        $orders = Order::where('user_id', $request->user()->id)->with('user', 'items.good.images,category')->orderBy('created_at', 'desc')->get();
 
 
         OrderResource::wrap('data');
@@ -116,7 +116,7 @@ class OrdersController extends Controller
                 $query->where('no', 'like', $like);
             });
 
-            $orders = $builder->with('user','items.good.images')->paginate(9);
+            $orders = $builder->with('user','items.good.images,category')->paginate(9);
 
             return new OrderResource($orders);
         }
